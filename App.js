9b9c84a4ac54f622ea66b75383861cef6d539a61
kyrrahnork <kyrrahnork@gmail.com>
2018-09-16 16:08:19 -0700

    adds 4 6 sided dice rolls

--- App.js
+++ App.js
@@ -14,19 +14,42 @@ export default class App extends Component {
   //   }
   // }
   state = {
-      strength : 9,
+      strength : this.GenerateAbilityScore(),
+      dexterity : this.GenerateAbilityScore(),
+      consitution : this.GenerateAbilityScore(),
+      intelligence : this.GenerateAbilityScore(),
+      wisdom : this.GenerateAbilityScore(),
+      charisma : this.GenerateAbilityScore()
+
+  }
+
+  GenerateAbilityScore(){
+    var firstDie = (Math.floor(Math.random()*6)+1);
+    var secondDie = (Math.floor(Math.random()*6)+1);
+    var thirdDie = (Math.floor(Math.random()*6)+1);
+    var forthDie = (Math.floor(Math.random()*6)+1);
+    var lowest = Math.min(firstDie, secondDie, thirdDie, forthDie);
+    var sum = (firstDie + secondDie + thirdDie + forthDie) - lowest;
+
+    return(sum);
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.entry}>Enter score below: </Text>
         <TextInput
           onChangeText={(text) => this.setState({
             strength : text
           })}
         />
-        <Text style={styles.red}>Hello world!</Text>
+        <Text style={styles.green}>Hello world!</Text>
         <Text>Strength: {this.state.strength}</Text>
+        <Text>Dexterity: {this.state.dexterity}</Text>
+        <Text>Consitution: {this.state.consitution}</Text>
+        <Text>Intelligence: {this.state.intelligence}</Text>
+        <Text>Wisdom: {this.state.wisdom}</Text>
+        <Text>Charisma: {this.state.charisma}</Text>
       </View>
     );
   }
@@ -39,8 +62,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'skyblue',
   }, 
-  red: {
+  green: {
     backgroundColor: 'green',
     fontSize: 60,
+  },
+  entry: {
+    backgroundColor: 'red',
+    fontSize: 20,
+    color:'black',
   }
+
 });

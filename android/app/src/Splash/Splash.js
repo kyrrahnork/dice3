71106ef8aadddf71d158431de7855d11835fc193
kyrrahnork <kyrrahnork@gmail.com>
2018-09-18 20:50:25 -0700

    adds button to first screen
    Added the start button and started to add the ability to generate ability scores between 8 and 18.

--- android/app/src/Splash/Splash.js
+++ android/app/src/Splash/Splash.js
@@ -1,20 +1,105 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
     View,
     Text,
-    Image
+    Image,
+    Button,
+    StyleSheet
 } from 'react-native';
 
-export default class SplashScreen extends React.Component{
+export default class BackgroundImage extends Component{
     static navigationOptions={
         header:null
     };
 
-    render(){
-        return(
-            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Image source={require('../main/res/drawable/screen.png')} />
-            </View>
+    // render(){
+    //     return(
+    //             <Image source={require('../main/res/drawable/screen.png')}
+    //                 style={styles.backgroundImage}>
+
+    //                 {this.props.children}    
+
+    //             </Image>
+    //     )
+    // }
+
+    render() {
+        const resizeMode = 'center';
+    const text = 'I am some centered text';
+
+    return (
+    <View
+        style={{
+        flex: 1,
+        backgroundColor: '#eee',
+        }}
+    >
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Image
+            style={{
+              flex: 1,
+              resizeMode: 'cover',
+              height: null, 
+              width: null,
+            }}
+            source={require('../main/res/drawable/screen.png')}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            justifyContent: 'center',
+          }}
+        >
+          <Button
+            style={{
+              textAlign: 'center',
+            
+              fontSize: 40,
+              color: 'red'
+            }}
+            title='press to begin'
+            onPress={()=> this.props.navigation.navigate('Start')}
+          >
+            {text}
+          </Button>
+        </View>
+    </View>
+    )
+    }
+}
+
+class TestBackgroundImage extends Component{
+    render() {
+        return (
+            <BackgroundImage>
+                <Text style={styles.text}>button goes here</Text>
+            </BackgroundImage>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    backgroundImage:{
+        flex:1,
+        width:null,
+        height:null,
+        resizeMode:'cover'
+    },
+
+    // text: {
+    //     textAlign:'center',
+    //     color:'white',
+    //     backgroundColor: 'rgba(0,0,0,0)',
+    //     fontSize:32
+    // }
+});
--- android/app/src/Start/Start.js
+++ android/app/src/Start/Start.js
@@ -5,11 +5,45 @@ import {
 } from 'react-native';
 
 export default class StartScreen extends React.Component{
+
+    state = {
+        str : 0
+    }
+
+    componentDidMount(){
+       let x = this.GenerateAbilityScore();
+        this.setState({str:x})
+    }
+
     render(){
+        console.log("hi");
         return(
             <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>StartScreen</Text>
+                <Text>StartScreen, Str = {this.state.str}</Text>
             </View>
         )
     }
+
+    RollDie = (dieValue) => {
+        return Math.floor(Math.random() * dieValue)+1;
+    }
+
+    GenerateAbilityScore()
+    {
+        do{
+            var firstDie = this.RollDie(6);
+            var secondDie = this.RollDie(6);
+            var thirdDie = this.RollDie(6);
+            var fourthDie = this.RollDie(6);
+            var lowest = Math.min(firstDie, secondDie, thirdDie, fourthDie);
+        
+            var dieTotal = (firstDie + secondDie + thirdDie + fourthDie) - lowest;
+
+        }while(dieTotal < 8)
+        
+
+        return dieTotal;
+    }
+
+
 }
